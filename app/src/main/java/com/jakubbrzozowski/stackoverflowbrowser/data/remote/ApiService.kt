ccb8418500a8ff866d7e7000f024d30af3f8b84f
Jakub Brzozowski <kubab.285@gmail.com>
2018-11-10 07:46:27 +0100

    Add search endpoint

--- app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/remote/ApiService.kt
+++ app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/remote/ApiService.kt
@@ -1,9 +1,21 @@
 package com.jakubbrzozowski.stackoverflowbrowser.data.remote
 
+import com.jakubbrzozowski.stackoverflowbrowser.data.model.Questions
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
     companion object {
         private const val API_VERSION = "2.2"
         const val URL = "http://api.stackexchange.com/$API_VERSION/"
     }
+
+    @GET("/search")
+    fun searchRepositories(
+            @Query("intitle") intitle: String,
+            @Query("site") site: String,
+            @Query("page") page: Int
+    ): Single<Questions>
 }
