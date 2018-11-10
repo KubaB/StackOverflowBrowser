32bf9f3364a3a26fe5a09cbc66a750bcdcba6b8c
Jakub Brzozowski <kubab.285@gmail.com>
2018-11-10 07:45:58 +0100

    Add questions data model

---
+++ app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/model/Owner.kt
@@ -0,0 +1,27 @@
+package com.jakubbrzozowski.stackoverflowbrowser.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Owner(
+
+	@field:SerializedName("profile_image")
+	val profileImage: String? = null,
+
+	@field:SerializedName("user_type")
+	val userType: String? = null,
+
+	@field:SerializedName("user_id")
+	val userId: Int? = null,
+
+	@field:SerializedName("link")
+	val link: String? = null,
+
+	@field:SerializedName("reputation")
+	val reputation: Int? = null,
+
+	@field:SerializedName("display_name")
+	val displayName: String? = null,
+
+	@field:SerializedName("accept_rate")
+	val acceptRate: Int? = null
+)
---
+++ app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/model/Questions.kt
@@ -0,0 +1,18 @@
+package com.jakubbrzozowski.stackoverflowbrowser.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Questions(
+
+	@field:SerializedName("quota_max")
+	val quotaMax: Int? = null,
+
+	@field:SerializedName("quota_remaining")
+	val quotaRemaining: Int? = null,
+
+	@field:SerializedName("has_more")
+	val hasMore: Boolean? = null,
+
+	@field:SerializedName("items")
+	val items: List<Question?>? = null
+)
