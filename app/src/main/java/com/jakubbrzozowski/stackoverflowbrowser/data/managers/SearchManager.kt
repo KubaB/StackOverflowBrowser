f3a91e912dc70a9b1be32fae12d445c199b1813d
Jakub Brzozowski <kubab.285@gmail.com>
2018-11-11 14:28:51 +0100

    Split SearchManager into interface and its implementation

--- app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/managers/SearchManager.kt
+++ app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/data/managers/SearchManager.kt
@@ -1,55 +1,10 @@
 package com.jakubbrzozowski.stackoverflowbrowser.data.managers
 
 import com.jakubbrzozowski.stackoverflowbrowser.data.model.remote.Question
-import com.jakubbrzozowski.stackoverflowbrowser.data.remote.ApiService
-import com.jakubbrzozowski.stackoverflowbrowser.data.repository.QuestionsRepository
 import com.jakubbrzozowski.stackoverflowbrowser.utils.PositionAndCount
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
-import timber.log.Timber
-import java.util.*
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class SearchManager @Inject
-constructor(private val apiService: ApiService,
-            private val questionsRepository: QuestionsRepository) {
-    companion object {
-        const val DEFAULT_SITE = "stackoverflow"
-        const val STARTING_PAGE = 1
-    }
-
-    private var searchResults: MutableList<Question?> = Collections.emptyList()
-    private var pageCounter = STARTING_PAGE
-
-    fun searchQuestions(q: String): Single<List<Question?>?> {
-        pageCounter = STARTING_PAGE
-        searchResults.clear()
-        return searchQuestions(q, STARTING_PAGE)
-    }
-
-    fun loadNextPage(q: String): Single<PositionAndCount> {
-        return Single.create { emitter ->
-            val lastPosition = searchResults.size - 1
-            searchQuestions(q, ++pageCounter)
-                    .subscribe(
-                            {
-                                emitter.onSuccess(PositionAndCount(
-                                        lastPosition,
-                                        searchResults.size - lastPosition - 1))
-                            },
-                            { ex -> Timber.e(ex) })
-        }
-    }
-
-    private fun searchQuestions(q: String, page: Int): Single<List<Question?>?> {
-        return apiService.searchQuestions(q, DEFAULT_SITE, page)
-                .map { questions ->
-                    if (page == STARTING_PAGE) questionsRepository.deleteAllQuestions()
-                    questions.items?.let { questionsRepository.saveQuestions(it) }
-                    questionsRepository.getAllQuestions()
-                }
-                .subscribeOn(Schedulers.io())
-    }
+interface SearchManager {
+    fun searchQuestions(q: String): Single<List<Question?>?>
+    fun loadNextPage(q: String): Single<PositionAndCount>
 }
--- app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/injection/module/ApplicationModule.kt
+++ app/src/main/java/com/jakubbrzozowski/stackoverflowbrowser/injection/module/ApplicationModule.kt
@@ -2,6 +2,9 @@ package com.jakubbrzozowski.stackoverflowbrowser.injection.module
 
 import android.app.Application
 import android.content.Context
+import com.jakubbrzozowski.stackoverflowbrowser.data.managers.SearchManager
+import com.jakubbrzozowski.stackoverflowbrowser.data.managers.SearchManagerImpl
+import com.jakubbrzozowski.stackoverflowbrowser.data.remote.ApiService
 import com.jakubbrzozowski.stackoverflowbrowser.data.repository.QuestionsRepository
 import com.jakubbrzozowski.stackoverflowbrowser.data.repository.QuestionsRepositoryImpl
 import com.jakubbrzozowski.stackoverflowbrowser.injection.qualifier.ApplicationContext
@@ -39,5 +42,11 @@ class ApplicationModule(val application: Application) {
     @Singleton
     fun provideQuestionRepository(): QuestionsRepository {
         return QuestionsRepositoryImpl()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchManager(apiService: ApiService, questionsRepository: QuestionsRepository): SearchManager {
+        return SearchManagerImpl(apiService, questionsRepository)
     }
 }
